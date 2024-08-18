c89fbd9db6d9461cd921c03e9f395eb4898237cf
Chloe Houvardas <chloehouvardas404@gmail.com>
2024-08-18 18:17:26 -0400

    untracked config.js

--- Backend/config.js
+++
@@ -1,4 +0,0 @@
-// Define the port which this server will listen for requests in config.js
-export const PORT = 5555;
-
-export const mongoDbURL = 'mongodb+srv://admin:admin@cluster0.gleez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
